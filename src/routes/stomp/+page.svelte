a6aeb7ec0673a4502a4ce483d54d90d056ba9659
jiyemun <jym6853@gmail.com>
2024-09-10 16:41:11 +0900

    Change: stomp connect, disconnect button add

--- src/routes/stomp/+page.svelte
+++ src/routes/stomp/+page.svelte
@@ -2,27 +2,43 @@
     import SockJS from 'sockjs-client';
     import Stomp from 'stompjs';
 
-    // // SockJS를 사용하여 웹소켓 연결 설정
-    const socket = new SockJS('http://192.168.10.206:12067/stream');
-    // 연결이 되면 url 표기도 변경됨 -> ws://192.168.10.206:12067/stream/216/zboq1ww3/websocket
-    const stompClient = Stomp.over(socket);
+    let socket = null;
+    let stompClient = null;
 
-    // 연결 시작
-    stompClient.connect({}, (frame) => {
-        console.log('Connected: ' + frame);
+    function connectSocket() {
+        // // SockJS를 사용하여 웹소켓 연결 설정
+        socket = new SockJS('http://192.168.10.206:12067/stream');
+        console.log(socket,"socket")
 
-        // 메시지 구독
-        stompClient.subscribe('/sub/state/outer/device', (message) => {
-            console.log('Received: ' + message.body);
+        // 연결이 되면 url 표기도 변경됨 -> ws://192.168.10.206:12067/stream/216/zboq1ww3/websocket
+        stompClient = Stomp.over(socket);
+        console.log(stompClient,"stompClient")
+        // 연결 시작
+        stompClient.connect({}, (frame) => {
+            console.log('Connected: ' + frame);
+
+            // 메시지 구독
+            stompClient.subscribe('/sub/state/outer/device', (message) => {
+                console.log('Received: ' + message.body);
+            });
+
+            // 메시지 발행
+            stompClient.send("/pub/sendMessage", {}, JSON.stringify({
+                message: "Hello, Server!"
+            }));
+        }, (error) => {
+            console.log(error, "error")
         });
+    }
 
-        // 메시지 발행
-        stompClient.send("/pub/sendMessage", {}, JSON.stringify({
-            message: "Hello, Server!"
-        }));
-    }, (error) => {
-        console.log(error)
-    });
+    function disConnectSocket() {
+        if(socket != null && stompClient != null){
+            stompClient.disconnect(()=>{
+                socket = null;
+                stompClient = null;
+            });
+        }
+    }
 
 </script>
 
@@ -48,4 +64,6 @@
             기능 : 구독/발행 패턴을 통한 메시지 관리, 메시지 헤더 처리, 연결 해제 등의 작업을 쉽게 처리
         </li>
     </ol>
+    <button on:click={connectSocket}>connect</button>
+    <button on:click={disConnectSocket}>disConnect</button>
 </div>
